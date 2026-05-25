37eca569e51c1b03097164f80e74a63e21bb2981
mateuszkoxpvp <157216372+mateuszkoxpvp@users.noreply.github.com>
2026-05-25 02:37:46 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,41 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import asyncio
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+PAYMENT_LINK = "TUTAJ_WKLEJ_LINK_STRIPE"
+
+@dp.message(Command("start"))
+async def start(message: types.Message):
+
+    telegram_id = message.from_user.id
+
+    payment_url = f"{PAYMENT_LINK}?client_reference_id={telegram_id}"
+
+    kb = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="Kup VIP",
+                    url=payment_url
+                )
+            ]
+        ]
+    )
+
+    await message.answer(
+        "Kup dostęp VIP:",
+        reply_markup=kb
+    )
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
